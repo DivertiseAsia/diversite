f961e9d35395ced0624c4ac8eeb217f5e9e2f27f
Hunter Williams <hunter@huntergwilliams.com>
2019-02-04 12:58:15 +0700

    fix builds locally

--- webpack.prod.js
+++ webpack.prod.js
@@ -9,7 +9,6 @@ const HtmlWebpackExcludeAssetsPlugin = require('html-webpack-exclude-assets-plug
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const environments = require('./environments');
 
 module.exports = merge(common, {
   mode: 'production',
