e967198f918e3ca70aae8e74041a38c510d18915
Hunter Williams <hgw2es@virginia.edu>
2021-08-27 16:15:10 +0700

    add in next.js

---
+++ next.config.js
@@ -0,0 +1,57 @@
+const bsconfig = require("./bsconfig.json");
+const fs = require("fs");
+const path = require("path");
+
+const transpileModules = ["rescript"].concat(bsconfig["bs-dependencies"]);
+const withTM = require("next-transpile-modules")(transpileModules);
+
+const isWebpack5 = true;
+const config = {
+  target: "serverless",
+  pageExtensions: ["jsx", "js"],
+  env: {
+    ENV: process.env.NODE_ENV,
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  webpack: (config, options) => {
+    const { isServer } = options;
+
+    if (isWebpack5) {
+      if (!isServer) {
+        // We shim fs for things like the blog slugs component
+        // where we need fs access in the server-side part
+        config.resolve.fallback = {
+          fs: false,
+          path: false,
+        };
+      }
+
+      // We need this additional rule to make sure that mjs files are
+      // correctly detected within our src/ folder
+      config.module.rules.push({
+        test: /\.m?js$/,
+        use: options.defaultLoaders.babel,
+        exclude: /node_modules/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
+        }
+      });
+    //   config.module.rules.push({
+    //     test: /\.(jpe?g|png|gif|svg|pdf|ico)$/,
+    //       use: [{
+    //         loader: 'file-loader',
+    //         options: {
+    //           name: '[path][name]-[hash:6].[ext]'
+    //         },
+    //       }, ]
+    //   });
+    }
+    return config
+  },
+  webpack5: isWebpack5
+};
+
+module.exports = (config);
