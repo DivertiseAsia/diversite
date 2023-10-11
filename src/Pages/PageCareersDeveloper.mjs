8e3dd2ffbff87035ed4f7cec3e82b21bc66a7904
Jaydee <rutchadapon.au22@hotmail.com>
2023-10-11 09:51:01 +0700

    Apply button

--- src/Pages/PageCareersDeveloper.mjs
+++ src/Pages/PageCareersDeveloper.mjs
@@ -39,7 +39,7 @@ function PageCareersDeveloper(Props) {
                               className: "align-self-center"
                             }, "What will I be doing?")), React.createElement("ul", {
                           className: "ul-li-padding-none ul-li-dash p-4 pl-5 -lineheight-large"
-                        }, React.createElement("li", undefined, "Working in application/AI projects"), React.createElement("li", undefined, "Working in fullstack development from deployment on AWS, frontend in Reason, and backend in Python/Django"), React.createElement("li", undefined, "Hybrid work at BTS Chidlom station once/two times a week 10:00-18:00"))), React.createElement("div", {
+                        }, React.createElement("li", undefined, "Working in application/AI projects"), React.createElement("li", undefined, "Working in fullstack development from deployment on AWS, frontend in Rescript, and backend in Python/Django"), React.createElement("li", undefined, "Hybrid work at BTS Chidlom station once/two times a week at 10:00-18:00 hr."))), React.createElement("div", {
                       className: "row pt-5 d-block"
                     }, React.createElement("div", {
                           className: "d-flex flex-row"
@@ -50,7 +50,12 @@ function PageCareersDeveloper(Props) {
                               className: "align-self-center"
                             }, "When can I start?")), React.createElement("div", {
                           className: "ul-li-padding-none p-4 pl-5"
-                        }, React.createElement("p", undefined, "It is recommended to apply 1-2 months prior to start date.")))), React.createElement("div", {
+                        }, React.createElement("p", undefined, "It is recommended to apply 1-2 months prior to start date."))), React.createElement("div", {
+                      className: "text-center"
+                    }, React.createElement("a", {
+                          className: "btn",
+                          href: "https://divertise.co.th/assignments/developer"
+                        }, "Apply"))), React.createElement("div", {
                   className: "footer-intern bg-gradient-color2"
                 }, React.createElement("div", {
                       className: "container pt-5 pb-4"
